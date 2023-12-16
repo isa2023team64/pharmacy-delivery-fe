4f33ff101189bab2fc35d44b2ce160832a600cde
Veljko121 <109314562+Veljko121@users.noreply.github.com>
2023-12-16 19:54:29 +0100

    Update equipment.model.ts

--- src/app/infrastructure/rest/model/equipment.model.ts
+++ src/app/infrastructure/rest/model/equipment.model.ts
@@ -5,7 +5,6 @@ export class Equipment{
     type: string;
     stockCount: number;
     averageRating?:string;
-    stockCount?: number;
 
 
     constructor(name: string, description: string, type: string, stockCount: number) {
