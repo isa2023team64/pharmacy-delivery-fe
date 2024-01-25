664924f181d5bc4e12fc4d4a1a5f72117efa04cc
milossdjuric <milossdjuric@yahoo.com>
2024-01-25 23:29:39 +0100

    feat: Add Stomp WebSocket

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -4,6 +4,9 @@ import { CompanyService } from '../../company/company.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import * as L from 'leaflet';
 import { Coordinates } from '../../infrastructure/rest/model/coordinates.model';
+import {Stomp} from '@stomp/stompjs';
+import SockJS, * as WebSocketJS from 'sockjs-client';
+
 
 
 @Component({
@@ -15,14 +18,26 @@ export class MapDeliveryComponent implements OnInit, AfterViewInit {
 
   private map!: L.Map;
 
+  private serverUrl = "http://localhost:8080/ws";
+  private stompClient: any;
+
+  isLoaded: boolean = false;
+  isCustomSocketOpened: boolean = false;
+
+
+
   startCoordinates: Coordinates = new Coordinates(45.267136, 19.833549);
   endCoordinates: Coordinates = new Coordinates(45.21, 19.73);
+  coordinatesList: Coordinates[]=[];
 
+  constructor() { 
 
-  constructor() { }
+  }
 
   ngOnInit(): void {
-
+    // this.coordinatesList.push(this.startCoordinates);
+    // this.coordinatesList.push(this.endCoordinates);
+    this.initializeWebSocketConnection();
   }
 
   ngAfterViewInit(): void { 
@@ -81,9 +96,72 @@ export class MapDeliveryComponent implements OnInit, AfterViewInit {
     delivery.bindPopup('Delivery').openPopup();
   }
 
-  private startDelivery():void{
+  public startDelivery(): void{
 
+    const coordinatesList: Coordinates[] = [this.startCoordinates, this.endCoordinates];
+    console.log(coordinatesList);
+
+    // if (!this.stompClient.connected) {
+    //   console.error('WebSocket connection is not established.');
+    //   return;
+    // }
+
+    this.sendMessageUsingSocket();
+    
   }  
 
+
+  initializeWebSocketConnection() {
+    // serverUrl je vrednost koju smo definisali u registerStompEndpoints() metodi na serveru
+    let ws = new SockJS(this.serverUrl);
+    this.stompClient = Stomp.over(ws);
+    let that = this;
+
+    const headers = { 'Authorization': `Bearer ${localStorage.getItem("jwt")}` };
+
+    this.stompClient.configure({
+      websocket: true,
+      debug: (str: string) => {
+          console.log(str);
+      },
+      headers: headers,
+      withCredentials: true  // Set withCredentials to true
+  });
+
+    this.stompClient.connect(headers, function () {
+      that.isLoaded = true;
+      that.openGlobalSocket()
+    });
+
+  }
+
+  sendMessageUsingSocket() {
+      const message = "Proba";
+      
+      this.stompClient.send("/ws-subscriber/delivery", {}, JSON.stringify(message));
+  }
+
+  openGlobalSocket() {
+    if (this.isLoaded) {
+      this.stompClient.subscribe("/ws-publisher", (message: { body: string; }) => {
+        this.handleResult(message);
+      });
+    }
+  }
+
+  openSocket() {
+    if (this.isLoaded) {
+      this.isCustomSocketOpened = true;
+      this.stompClient.subscribe("/ws-publisher/coordinates", (message: { body: string; }) => {
+        this.handleResult(message);
+      });
+    }
+  }
+
+  handleResult(message: { body: string; }) {
+    if (message.body) {
+      let messageResult: string = JSON.parse(message.body);
+    }
+  }
 
 }
